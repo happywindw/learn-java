5b568d4f8123d198dcc6d238eb9e4ce055a6bd08
happywindw <592595468@qq.com>
2018-04-17 12:33:57 +0800

    增加3.9小节内容要点与示例代码

---
+++ corejava/chapter03/BigIntegerTest.java
@@ -0,0 +1,29 @@
+package chapter03;
+
+import java.math.*;
+import java.util.*;
+
+/**
+ * This program use big numbers to compute the odds of winning the grand prize in a lottery.
+ */
+public class BigIntegerTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many numbers do you need to draw? ");
+        int k = in.nextInt();
+
+        System.out.print("What is the highest number you can draw?　");
+        int n = in.nextInt();
+
+        /*
+        compute binomial coefficient n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k)
+        */
+        BigInteger lotteryOdds = BigInteger.valueOf(1);
+        for(int i = 1; i <= k; i++) {
+            lotteryOdds = lotteryOdds.multiply(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i));
+        }
+
+        System.out.printf("Your odds are 1 in %d. Good luck!", lotteryOdds);
+    }
+}
