374dc3e5727a038eb87c65d6d3b07391ca640fd1
happywindw <592595468@qq.com>
2018-04-24 23:55:49 +0800

    增加5.2小节关于equals的示例代码

---
+++ corejava/chapter05/equals/Manager.java
@@ -0,0 +1,37 @@
+package chapter05.equals;
+
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String n, double s, int year, int month, int day) {
+        super(n, s, year, month, day);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b) {
+        bonus = b;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if(!super.equals(otherObject)) return false;
+        Manager other = (Manager)otherObject;
+        //super.equals checked that this and other belong to same class
+        return bonus == other.bonus;
+    }
+
+    public int hashCode() {
+        return super.hashCode() + 17 * new Double(bonus).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "[bonus=" + bonus + "]";
+    }
+}
